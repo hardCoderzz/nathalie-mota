0ba5c1301e20744fc8e16b492e93fe70188d4ddc
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-06-29 19:47:39 +0200

    Create contact.php
    Template modale en tant que template part

---
+++ template-parts/contact-popup/contact.php
@@ -0,0 +1,8 @@
+<div id="contact-popup" class="popup">
+    <div class="popup-wrapper">
+        <div class="popup-content">
+            <img class="contact-text" src="<?php echo get_template_directory_uri() . '/assets/images/contact-header.png'?>" alt="Contact">
+            <?php echo do_shortcode('[contact-form-7 id="27" title="Contact form Nathalie Mota"]'); ?>
+        </div>
+    </div>  
+</div>
