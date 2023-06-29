fef825f313a0414e30e6ae82ccd718ec26b03dc8
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-06-29 19:39:15 +0200

    Update footer.php
    ajout copyright texte dans le footer

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
     <?php wp_nav_menu([
         'theme_location' => 'footer',
         'container' => false,
-        'menu_class' => 'footer-menu'
+        'menu_class' => 'footer-menu',
+        'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s<li class="copyright">Tous droits réservés</li></ul>'
     ])
-    ?>
     
+    ?>
     <?php wp_footer() ?>
+
 </body>
 </html>
