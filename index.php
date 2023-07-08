e5c1bc8ccd3e79b15b408a9eea96992b2bb7747d
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-07-08 02:04:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
-<?php get_header(); ?>  
+<?php get_header(); ?> 
+
 <?php get_template_part('template-parts/contact-popup/contact'); ?>
+
+<?php get_template_part('template-parts/content/single-photo'); ?>
+
 <?php get_footer(); ?>
