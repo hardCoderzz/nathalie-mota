1209c418856883827173086b6d847d64865bdc3f
Soufiane Soultani <soufiane.soultani@tutanota.com>
2023-07-14 00:01:10 +0200

    Update contact.js

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,9 +1,16 @@
-const contactLink = document.querySelector('.contact-btn');
+const contactLinks = document.querySelectorAll('.contact-btn, .contact-single-btn');
 const overlay = document.querySelector('.popup');
 const popup = document.querySelector('.popup-content');
+const form = document.querySelector('.wpcf7');
 
-contactLink.addEventListener('click', () => {
-    overlay.classList.add('open');
+contactLinks.forEach((contactLink) => {
+    contactLink.addEventListener('click', () => {
+        overlay.classList.add('open');
+    });
+});
+
+form.addEventListener('click', (event) => {
+    event.stopPropagation();
 });
 
 overlay.addEventListener('click', () => {
